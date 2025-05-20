5886bb295fc82b50738b8f7b84cefcb09f33a80e
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2025-05-20 13:29:49 +0200

    Please use my math 😭

--- src/View/ButtonView.cpp
+++ src/View/ButtonView.cpp
@@ -1,6 +1,7 @@
 #include "../../include/View/ButtonView.h"
 #include "../../include/utils/TextHelper.h" // For TextHelper functions
 #include <iostream> // For std::cer
+#define _USE_MATH_DEFINES
 #include <cmath>
 
 ButtonView::ButtonView() : font_(nullptr) {
